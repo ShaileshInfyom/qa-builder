aae8c046eeabb273cb78518915195c8207d5c4b6
ShaileshInfyom <shailesh.infyom@gmail.com>
2022-01-08 05:46:56 +0000

    Code Synced by DhiWise

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -40,6 +40,47 @@ class Category extends Model
         'updated_by' => 'integer',
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $existingModel = $model->orderBy('id', 'desc')->first();
+            static::createNameString($existingModel, $model);
+        });
+
+        static::updating(function ($model) {
+            static::updateNameString($model);
+        });
+    }
+
+    /**
+     * @param $existingModel
+     * @param $creatingModel
+     *
+     * @return string
+     */
+    public static function createNameString($existingModel, $creatingModel): string
+    {
+        $series = 5;
+        if (!empty($existingModel)) {
+            preg_match('~SML(.*?)LADU~', $existingModel->name, $output);
+            $series = $output[1] + 1;
+        }
+
+        return $creatingModel->name = 'SML'.$series.'LADU';
+    }
+
+    /**
+     * @param $updatingModel
+     *
+     * @return mixed
+     */
+    public static function updateNameString($updatingModel)
+    {
+        return $updatingModel->name = $updatingModel->where('id', $updatingModel->id)->first()->name;
+    }
+
     protected static $logAttributes = ['id', 'name', 'description', 'is_active', 'created_at', 'updated_at', 'added_by', 'updated_by'];
 
     /**
--- database/seeders/DefaultUserSeeder.php
+++ database/seeders/DefaultUserSeeder.php
@@ -58,9 +58,9 @@ class DefaultUserSeeder extends Seeder
             'is_active' => $faker->boolean(true),
             'created_at' => $faker->dateTime(),
             'updated_at' => $faker->dateTime(),
-            'username' => 'domenico33',
-            'email' => 'joan.roob@yahoo.com',
-            'password' => Hash::make('Tl=cSV#8l'),
+            'username' => 'bednar.sophia',
+            'email' => 'rath.josefa@boyer.com',
+            'password' => Hash::make('xd1hNOI|'),
             'email_verified_at' => Carbon::now(),
             'user_type' => User::TYPE_ADMIN
         ];
